9e27f3e12b647dd7baeec8d01d4c5a4b9f7a5483
borisgern <borisgern@gmail.com>
2018-04-24 19:22:46 +0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,32 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({name: 'Boris'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({_id: new ObjectID('5adf57a30ad45e2bb0b5e8f4')}).then((result) => {
+    console.log(result);
+  });
+
+//  client.close();
+});

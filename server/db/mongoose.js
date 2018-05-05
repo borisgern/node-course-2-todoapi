60ba3f5976b25821af5914693c5122741a932303
borisgern <borisgern@gmail.com>
2018-05-05 18:27:24 +0200

    setup app for heroku with correct order of db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api' || 'mongodb://localhost:27017/TodoApp');
-
+mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api');
 module.exports = {mongoose};

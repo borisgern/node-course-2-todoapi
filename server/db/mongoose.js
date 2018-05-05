5862ac16dd8287afda58e651c8cce1a7a14ec63e
borisgern <borisgern@gmail.com>
2018-05-05 18:05:40 +0200

    setup app for heroku final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api' || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api');
+mongoose.connect('mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};

fc05ca94c6ba8f2d29d2a9af19041463ebd8fb3f
borisgern <borisgern@gmail.com>
2018-05-06 15:55:46 +0200

    test of heroku bag v.0.1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api');
-mongoose.connect('mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api' || 'mongodb://localhost:27017/TodoApp');
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api'
+};
+mongoose.connect(process.env.PORT ?  db.mlab: db.localhost);
 module.exports = {mongoose};

048b070c53de450cbb8ba97c5aac7bfa66c0b5f3
borisgern <borisgern@gmail.com>
2018-05-06 15:58:38 +0200

    heroku bag fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api');
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://borisgern:1@ds117749.mlab.com:17749/test-todo-app-api'
